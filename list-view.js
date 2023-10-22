61c76b0af1c4f609ac15a7fd4d325b6ff51bd7ae
mrestrepoj2 <mrestrepoj2@gmail.com>
2023-10-21 19:18:53 -0500

    app.js

--- list-view.js
+++ list-view.js
@@ -1,4 +1,3 @@
 // Crea un archivo llamado list-view-router. En este archivo crea una instancia de Express Router que permita:
 // Hacer una solicitud GET a una ruta específica para listar las tareas que están completas.
 // Hacer una solicitud GET a una ruta específica para listar las tareas que están incompletas.
-
